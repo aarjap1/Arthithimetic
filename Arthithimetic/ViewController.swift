5d80e0e94cee6deaaf9b6e43237ce04303dd799b
Piya,Aarjap Kumar <s528372@ch140005m.nwmissouri.edu>
2019-02-14 18:01:32 -0600

    Initial Commit

---
+++ Arthithimetic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arthithimetic
+//
+//  Created by Piya,Aarjap Kumar on 2/14/19.
+//  Copyright © 2019 Piya,Aarjap Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
